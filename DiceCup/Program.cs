5742392788e737f9e3b24cffb3cb8d1edf188857
Erik <erik.ra@gmail.com>
2017-04-26 00:46:54 +0200

    Add project files.

---
+++ DiceCup/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceCup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> stats = new Dictionary<int, int>();
+
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int dice1 = int.Parse(inputs[0]);
+            int dice2 = int.Parse(inputs[1]);
+
+            for (int i = 1; i <= dice1; i++)
+            {
+                for (int ii = 1; ii <= dice2; ii++)
+                {
+                    int sum = i + ii;
+                    int value;
+                    if (stats.TryGetValue(sum, out value))
+                    {
+                        stats[sum] += 1;
+                    }
+                    else
+                    {
+                        stats.Add(sum, 1);
+                    }
+                }
+            }
+
+            var maxKvP = stats.GroupBy(kv => kv.Value).OrderByDescending(a => a.Key).First();
+
+            foreach (var kvp in maxKvP)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+        }
+    }
+}
